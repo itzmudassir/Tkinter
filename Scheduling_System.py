c46d1aa9b31dfc431f3814f6ef2a32cd17bb9dc4
itzmudassir <itzmudassir07@gmail.com>
2022-11-29 11:03:47 +0500

    Created three Checkboxes

--- Scheduling_System.py
+++ Scheduling_System.py
@@ -24,6 +24,108 @@ app.wm_attributes('-fullscreen','True')
 app.configure(bg="white")
 
 def home():
+    # Globalizing the variables
+    global i 
+    global j
+    global k
+    global h1
+    global m1
+    global s1
+    global h2 
+    global m2
+    global s2
+    global h3
+    global m3
+    global s3
+    global h11
+    global m11
+    global s11
+    global h22
+    global m22
+    global s22
+    global h33
+    global m33
+    global s33
+   
+    i = 0
+    j = 0
+    k = 0
+    h1= 0
+    m1 = 0
+    s1 = 0
+    h2 = 0
+    m2 = 0
+    s2 = 0
+    h3 = 0
+    m3 = 0
+    s3 = 0
+    h11 = 0
+    m11 = 0
+    s11 = 0
+    h22 = 0
+    m22 = 0
+    s22 = 0
+    h33 = 0
+    m33 = 0
+    s33 = 0
+    
+    global checkbox_1    # Making the checkbox_1 global
+    global checkbox_2    # Making the checkbox_2 global
+    global checkbox_3    # Making the checkbox_3 global
+    
+    global label_1       # Making the label_1 global   
+    global label_2       # Making the label_2 global
+    global label_3       # Making the label_3 global
+    
+    global var1         # Making the var1 global
+    global var2         # Making the var2 global
+    global var3         # Making the var3 global
+    
+    var1 = customtkinter.IntVar()       # Making the var1 as integer
+    var2 = customtkinter.IntVar()       # Making the var2 as integer
+    var3 = customtkinter.IntVar()       # Making the var3 as integer
+    
+    global dec_button_1   # Making the dec_button global
+    global inc_button_1  # Making the inc_button global
+    global value_label_1  # Making the value_label global
+    
+    
+    global dec_button_2  # Making the second dec_button global
+    global inc_button_2  # Making the second inc_button global
+    global value_label_2  # Making the second value_label global
+    
+    
+    global dec_button_3  # Making the third dec_button global
+    global inc_button_3  # Making the third inc_button global
+    global value_label_3  # Making the third value_label global 
+    
+    global hour_label_1  # Making the first hour_label global
+    global minute_label_1  # Making the first minute_label global
+    global second_label_1  # Making the first second_label global
+    
+    global hour_label_2  # Making the second hour_label global
+    global minute_label_2  # Making the second minute_label global
+    global second_label_2  # Making the second second_label global
+    
+    global hour_label_3  # Making the third hour_label global
+    global minute_label_3  # Making the third minute_label global
+    global second_label_3  # Making the third second_label global
+    
+    global hour_label_11  # Making the first hour_label global
+    global minute_label_11  # Making the first minute_label global
+    global second_label_11  # Making the first second_label global
+
+    global hour_label_22  # Making the second hour_label global
+    global minute_label_22  # Making the second minute_label global
+    global second_label_22  # Making the second second_label global
+    
+    global hour_label_33  # Making the third hour_label global
+    global minute_label_33  # Making the third minute_label global
+    global second_label_33  # Making the third second_label global
+    
+    global first_label_11  # Making the first_label_11 global
+    global second_label_11  # Making the second_label_11 global
+    global third_label_11  # Making the third_label_11 global
     
     # IMAGE SECTION STARTS HERE
     
@@ -56,6 +158,39 @@ def home():
                                             bg="white",
                                             fg_color="white")
     schedule_img.place(x=950, y=15)
+    
+    # CHECKBOX SECTION STARTS HERE
+    checkbox_1 = customtkinter.CTkCheckBox(master=app,
+                                           #command=checkbox_event_1,
+                                           variable=var1,
+                                           hover_color = "#2A2A9C",
+                                           fg_color = "#2A2A9C",
+                                           border_color = "black",
+                                           onvalue=1,
+                                           offvalue=0)                    
+    checkbox_1.place(x=10, y = 155)
+    
+    checkbox_2 = customtkinter.CTkCheckBox(master=app, 
+                                           #command=checkbox_event_2,
+                                           variable=var2,
+                                           hover_color = "#2A2A9C",
+                                           fg_color = "#2A2A9C",
+                                           border_color = "black",
+                                           onvalue=1,
+                                           offvalue=0)                    
+    checkbox_2.place(x=10, y = 285)
+    
+    checkbox_3 = customtkinter.CTkCheckBox(master=app, 
+                                           #command=checkbox_event_3,
+                                           variable=var3,
+                                           hover_color = "#2A2A9C",
+                                           fg_color = "#2A2A9C",
+                                           border_color = "black",
+                                           onvalue=1,
+                                           offvalue=0)                    
+    checkbox_3.place(x=10, y = 415)
+    # ********** END OF CHECKBOX SECTION **********
+    
     
     
 # Calling the home function
